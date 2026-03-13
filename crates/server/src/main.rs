87d32a11e1f19b4a513997b12719d6b6b3eba1d1
ShigekuniWork <shigekuni.work@gmail.com>
2026-03-13 22:55:57 +0900

    Use Tokio async main and start gRPC server

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,14 +1,26 @@
+use tokio_util::sync::CancellationToken;
 use tracing::info;
 
-use crate::{config::ServerConfig, logger::init_ocypode_logger};
+use crate::{config::ServerConfig, grpc::grpc_serve, logger::init_ocypode_logger};
 
 mod config;
+mod grpc;
 mod logger;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = ServerConfig::new();
     init_ocypode_logger(&config.logger);
 
     info!("Starting ocypode-server");
+
+    let cancel_token = CancellationToken::new();
+
+    // Setup gRPC server.
+    grpc_serve(&config.grpc, cancel_token).await;
+
     info!("Server is ready");
+
+    tokio::signal::ctrl_c().await?;
+    Ok(())
 }
